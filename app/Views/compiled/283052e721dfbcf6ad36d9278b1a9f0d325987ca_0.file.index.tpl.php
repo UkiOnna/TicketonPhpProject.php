c42dda9fbe12e3bf9e77d0f85fc53f75351e2223
Yernar <yernar8m@gmail.com>
2019-10-20 23:07:53 +0600

    corrected config.php

---
+++ app/Views/compiled/283052e721dfbcf6ad36d9278b1a9f0d325987ca_0.file.index.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2019-10-20 17:25:30
+  from 'C:\OSPanel\domains\test\app\Views\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5dac6e5abd1ec1_12933278',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '283052e721dfbcf6ad36d9278b1a9f0d325987ca' => 
+    array (
+      0 => 'C:\\OSPanel\\domains\\test\\app\\Views\\index.tpl',
+      1 => 1571576121,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5dac6e5abd1ec1_12933278 (Smarty_Internal_Template $_smarty_tpl) {
+?><h1><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</h1>
+<p>
+    <?php echo $_smarty_tpl->tpl_vars['message']->value;?>
+
+</p><?php }
+}
